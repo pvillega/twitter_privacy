efd79db342a38b20ecaf55e9644133d4f36d0fa7
Pere Villega <pvillega@aracon.com>
2019-01-23 21:12:11 +0000

    Add code to search for tweets older than certain amount of days in the user timeline

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate pretty_env_logger;
 extern crate tokio_core;
 
 mod config;
+use chrono::prelude::*;
+use chrono::Duration;
+use egg_mode::tweet;
 use tokio_core::reactor::Core;
 
 fn main() {
@@ -21,9 +24,8 @@ fn main() {
     let mut core = Core::new().unwrap();
     let config = config::Config::load(&mut core);
     // dbg!(&config);
-    
-    let handle = core.handle();
 
+    let handle = core.handle();
 
     let user_info = core
         .run(egg_mode::user::show(
@@ -34,4 +36,32 @@ fn main() {
         .unwrap();
 
     info!("{} (@{})", user_info.name, user_info.screen_name);
+
+    let timeline =
+        tweet::user_timeline(&user_info.id, true, true, &config.token, &handle).with_page_size(25);
+
+    process_timeline(&mut core, timeline);
+}
+
+fn process_timeline(mut core: &mut Core, timeline: tweet::Timeline) {
+    let future_timeline = timeline.older(None);
+    let (timeline, feed) = core.run(future_timeline).unwrap();
+
+    if feed.is_empty() {
+        info!("We got to the end of the timeline");
+    } else {
+        let utc: DateTime<Utc> = Utc::now();
+        for tweet in &feed {
+            if utc.signed_duration_since(tweet.created_at) > Duration::days(60) {
+                info!(
+                    "<@{}> [{}] {}",
+                    tweet.user.as_ref().unwrap().screen_name,
+                    tweet.created_at,
+                    tweet.text
+                );
+            }
+        }
+
+        process_timeline(&mut core, timeline);
+    }
 }
